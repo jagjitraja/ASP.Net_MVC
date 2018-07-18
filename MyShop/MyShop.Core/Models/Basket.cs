7ae75c41c4b98f1e30391038575e35b162ee2b86
Jagjit Bilkhu <jbilkhu@arrow.ca>
2018-07-18 13:53:45 -0700

    basket items

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseObjectEntity
+    {
+
+        //Virtual enable lazy loading to load all items inside the basket when basket is requested
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+           
+        }
+    }
+}
