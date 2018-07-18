759e77c8ce27889f9d174cd69beabf511e63acfa
Jagjit Bilkhu <jbilkhu@arrow.ca>
2018-07-18 09:17:14 -0700

    Created SQL Database

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        
+
+        //Casue the DbContext class to connect with the Database declared in the Web.Config file
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> DBProducts { get; set; }
+        public DbSet<ProductCategory> DBProductCategory { get; set; }
+    }
+}
